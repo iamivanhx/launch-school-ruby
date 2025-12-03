d568262b7852aee1e7dc376f3b5c203314a5a8c9
Ivan E. Hernandez T. <ivan@ivanhx.com>
2025-12-03 17:40:09 +0100

    The Basics - Ex1

--- RB100/the_basics/ex1.rb
+++ RB100/the_basics/ex1.rb
@@ -1,4 +1,9 @@
 # Add two strings together that, when concatenated, return your first and last name as your full name in one string.
 
-my_string = "Ivan" + " " + "Hernandez"
+my_string = "Fulano" + " " + "Detal"
 puts my_string
+
+first_name = "Fulano"
+last_name = "Detal"
+
+puts "#{first_name} #{last_name}"
