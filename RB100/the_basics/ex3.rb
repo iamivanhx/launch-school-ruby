a87e0d37de74a64bb33e98539a62d3c92435fb13
Ivan E. Hernandez T. <ivan@ivanhx.com>
2025-12-03 18:15:23 +0100

    The Basics - Ex3

---
+++ RB100/the_basics/ex3.rb
@@ -0,0 +1,15 @@
+# Write a program that uses a hash to store a list of movie titles with the year they came out. Then use the puts command to make your program print out the year of each movie to the screen.
+
+movies = {
+  Alien: 1979,
+  Aliens: 1986,
+  The_Shawshank_Redemption: 1994,
+  The_Godfather: 1972,
+  The_Dark_Knight: 2008
+}
+
+puts movies[:Alien]
+puts movies[:Aliens]
+puts movies[:The_Shawshank_Redemption]
+puts movies[:The_Godfather]
+puts movies[:The_Dark_Knight]
