22131cb0d60e6d5dcca470c94ecd2e56db680f85
Ivan E. Hernandez T. <ivan@ivanhx.com>
2025-12-03 18:27:28 +0100

    The Basics - Ex5

---
+++ RB100/the_basics/ex5.rb
@@ -0,0 +1,10 @@
+# Write a program that outputs the factorial of the numbers 5, 6, 7, and 8.
+factorial_5 = 5 * 4 * 3 * 2 * 1
+factorial_6 = 6 * 5 * 4 * 3 * 2 * 1
+factorial_7 = 7 * 6 * 5 * 4 * 3 * 2 * 1
+factorial_8 = 8 * 7 * 6 * 5 * 4 * 3 * 2 * 1
+
+puts "Factorial of 5 is: #{factorial_5}"
+puts "Factorial of 6 is: #{factorial_6}"
+puts "Factorial of 7 is: #{factorial_7}"
+puts "Factorial of 8 is: #{factorial_8}"
