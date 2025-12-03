784f25ae6b035d532f36afad5e7696ab1160b3a8
Ivan E. Hernandez T. <ivan@ivanhx.com>
2025-12-03 18:32:25 +0100

    The Basics - Ex6

---
+++ RB100/the_basics/ex6.rb
@@ -0,0 +1,8 @@
+# Write a program that calculates the squares of 3 float numbers of your choosing and outputs the result to the screen.
+number = 3.75
+number2 = 10.5
+number3 = 2.8
+
+puts "The square of #{number} is #{number * number}"
+puts "The square of #{number2} is #{number2 * number2}"
+puts "The square of #{number3} is #{number3 * number3}"
