413ef5bf71b69e336e962bfd63d556a7b75c464f
Ivan E. Hernandez T. <ivan@ivanhx.com>
2025-12-03 18:20:06 +0100

    The Basics - Ex4

---
+++ RB100/the_basics/ex4.rb
@@ -0,0 +1,8 @@
+# Use the dates from the previous example and store them in an array. Then make your program output the same thing as exercise 3.
+movies_year = [1979, 1986, 1994, 1972, 2008]
+
+puts movies_year[0]
+puts movies_year[1]
+puts movies_year[2]
+puts movies_year[3]
+puts movies_year[4]
