b3060280ab411246383ae4ee63e23e467d8d82c1
Ivan E. Hernandez T. <ivan@ivanhx.com>
2025-12-06 09:56:43 +0100

    Variables - Ex4

---
+++ RB100/variables/name3.rb
@@ -0,0 +1,7 @@
+# Modify name.rb again so that it first asks the user for their first name, saves it into a variable, and then does the same for the last name. Then outputs their full name all at once.
+print "Please type your first name: "
+first_name = gets.chomp
+print "Please type your last name: "
+last_name = gets.chomp
+
+puts "Hello! #{first_name} #{last_name}"
