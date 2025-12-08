b1d4fd2e3da2c86828de8dc42c0024e881079e35
Ivan E. Hernandez T. <ivan@ivanhx.com>
2025-12-08 18:18:45 +0100

    Methods - Ex5

---
+++ RB100/methods/ex5.rb
@@ -0,0 +1,9 @@
+# 1) Edit the method definition in exercise #4 so that it does print words on the screen. 2) What does it return now?
+# What will the following code print to the screen?
+def scream(words)
+  words = words + "!!!!"
+  puts words
+end
+
+p scream("Yippeee") # => Outputs Yippeee!!!! and returns nil
+
